86c12971fa9f52ddcaa90d718b43cf56282e7a72
Magnus Helle Kalland <magnushellekalland@gmail.com>
2022-10-16 11:07:30 +0200

    fix condition variable correctness

--- src/include/PacketQueue.hpp
+++ src/include/PacketQueue.hpp
@@ -20,29 +20,9 @@ public:
     }
     ~PacketQueue()
     {
-        /* TOOD: must be specific for type T    */
+        /* Should be specific for type T -      */
         /* this won't work for types with heap  */
         /* allocated members                    */
-        empty();
-    }
-    void push_msg(T t)
-    {
-        dq.push_front(t);
-        pthread_cond_broadcast(&cond);
-    }
-    T get_msg()
-    {
-        pthread_mutex_lock(&mutex);
-        if (dq.size() == 0) /* only one consumer, should't need a while loop */
-            pthread_cond_wait(&cond, &mutex);
-        pthread_mutex_unlock(&mutex);
-        T back = dq.back();
-        dq.pop_back();
-        return back;
-    }
-private:
-    void empty()
-    {
         T back;
         while (dq.size() > 0)
         {
@@ -50,5 +30,22 @@ private:
             dq.pop_back();
             free(back);
         }
+    }
+    void pusMessage(T t)
+    {
+        pthread_mutex_lock(&mutex);
+        dq.push_front(t);
+        pthread_mutex_unlock(&mutex);
+        pthread_cond_broadcast(&cond);
+    }
+    T popMessage()
+    {
+        pthread_mutex_lock(&mutex);
+        if (dq.size() == 0) /* only one consumer, this if should be fine */
+            pthread_cond_wait(&cond, &mutex);
+        T back = dq.back();
+        dq.pop_back();
+        pthread_mutex_unlock(&mutex);
+        return back;
     }
 };
